c4568b7166d5127a5b8510f2bf9ad971935f3da2
Patymaster <62792625+Patymaster@users.noreply.github.com>
2023-02-17 19:09:51 +0200

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication1 
+{
+    class Program
+    {
+        delegate int Average(int x, int y, int z);
+
+        static void Main(string[] args)
+        {
+            Average avg = delegate (int x, int y, int z)
+            {
+                return (x + y + z) / 3;
+            };
+
+            int result = avg(2, 4, 6);
+            Console.WriteLine("Average: {0}", result);
+        }
+    }
+}
